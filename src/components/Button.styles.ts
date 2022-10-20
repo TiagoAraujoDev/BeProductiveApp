33798b53bf14b7a2b4cab87f2cda99d72e008105
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-10-19 23:28:38 -0300

    Linting code and add eslint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,17 +1,10 @@
-import styled, { css } from "styled-components";
+import styled from 'styled-components'
 
-export type ButtonVariant = "primary" | "secundary" | "danger" | "success";
+export type ButtonVariant = 'primary' | 'secundary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
-
-const buttonVariants = {
-  primary: "purple",
-  secundary: "orange",
-  danger: "red",
-  success: "green",
-};
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
@@ -20,6 +13,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border-radius: 4px;
   margin: 8px;
 
-  background-color: ${(props) => props.theme["green-500"]};
-  color: ${(props) => props.theme.white}
-`;
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+`
