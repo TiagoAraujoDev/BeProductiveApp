93e84e318151a37212d6fada0c34be97a3c5d2cd
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-11-01 14:35:32 -0300

    formated date in history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
 import { useContext } from 'react'
 
 import { CycleContext } from '../../contexts/CyclesContext'
@@ -6,6 +8,15 @@ import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   const { cycles } = useContext(CycleContext)
+
+  function formatDate(date: Date): string {
+    const formatedDate = formatDistanceToNow(date, {
+      addSuffix: true,
+      locale: ptBr,
+    })
+    return formatedDate
+  }
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
@@ -25,7 +36,7 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>{formatDate(cycle.startDate)}</td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor={'green'}>Concluido</Status>
