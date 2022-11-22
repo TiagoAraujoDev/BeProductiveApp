95226916673958dd9ad63b56d9de02bca5d8a388
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-11-22 13:28:13 +0000

    change: Logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,15 @@
-import { CheckSquareOffset, Scroll, Timer } from 'phosphor-react'
+import { CheckSquareOffset, RocketLaunch, Scroll, Timer } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 
-import { HeaderContainer } from './styles'
-
-import igniteLogo from '../../assets/ignite-logo.svg'
+import { HeaderContainer, LogoContainer } from './styles'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>
-        <img src={igniteLogo} alt="" />
-      </span>
+      <LogoContainer>
+        <span>Focus</span>
+        <RocketLaunch size={32} color="#00875F" />
+      </LogoContainer>
       <nav>
         <NavLink to="/" end title="Timer">
           <Timer size={24} />
